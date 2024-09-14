2350fe3d50bc5ef984047a20028d03535caac815
Vinicios Neves <vinicios.neves@mac>
2024-09-14 18:21:20 -0300

    video 3.1

--- src/components/ChatBubble/index.jsx
+++ src/components/ChatBubble/index.jsx
@@ -1,11 +1,12 @@
 import styles from './chatBubble.module.css';
 import ReactMarkdown from 'react-markdown';
 
-export default function ChatBubble({ message, isUser = false }  ) {
+export default function ChatBubble({ message, onRemove, isUser = false }  ) {
   return (
     <div className={`${styles.bubbleWrapper} ${isUser ? styles.user : styles.bot}`}>
       <div className={styles.bubble}>
         <ReactMarkdown>{message}</ReactMarkdown>
+        <button className={styles.removeButton} onClick={onRemove}>remover ✖</button>
       </div>
     </div>
   );
--- src/components/ChatContainer/index.jsx
+++ src/components/ChatContainer/index.jsx
@@ -10,21 +10,24 @@ import { useChat } from 'ai/react';
 
 export const ChatContainer = () => {
 
-    const { messages, input, handleInputChange, handleSubmit, isLoading, stop } = useChat()
+    const { messages, setMessages, input, handleInputChange, handleSubmit, isLoading, stop } = useChat()
+    const handleDelete = (id) => {
+        setMessages(messages.filter(message => message.id !== id))
+    }
 
     return (
         <section className={styles.container}>
             <ChatHeader />
             <div className={styles.chat}>
-                
+
                 {messages.map((msg, index) => (
-                    <ChatBubble key={index} message={msg.content} isUser={msg.role === 'user'} />
+                    <ChatBubble key={index} message={msg.content} onRemove={() => handleDelete(msg.id)} isUser={msg.role === 'user'} />
                 ))}
 
             </div>
             {isLoading && <div>
                 <Loader />
-                <StopButton onClick={stop}/>
+                <StopButton onClick={stop} />
             </div>}
             <ChatForm
                 input={input}
